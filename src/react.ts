99f88825f36a823e2965ee04f597b13ed49349cb
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-08-26 17:39:10 +0200

    Add explicit diagrams client entrypoint

---
+++ src/react.ts
@@ -0,0 +1,24 @@
+"use client";
+
+// Explicit client-side diagram UI surface for donut-shaped consumers.
+export * from "./architecture-diagram";
+export * from "./decision-tree";
+export * from "./dependency-graph";
+export * from "./entity-relationship-diagram";
+export * from "./gantt-chart";
+export * from "./journey-map";
+export * from "./mind-map";
+export * from "./org-chart";
+export * from "./process-map";
+export * from "./relationship-map";
+export * from "./sequence-diagram";
+export * from "./state-machine-diagram";
+export * from "./swimlane-diagram";
+export * from "./timeline-diagram";
+export * from "./uml-diagram";
+export type {
+  DiagramEdgeInspectorContext,
+  DiagramInteractiveProps,
+  DiagramItemAction,
+  DiagramSearchResult,
+} from "./diagram-types";
