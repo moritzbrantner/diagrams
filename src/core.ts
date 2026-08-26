3e7f0a078a799a719537b13ee52838e6751e57e3
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-08-26 16:34:51 +0200

    Add server-safe diagrams core entrypoint

---
+++ src/core.ts
@@ -0,0 +1,13 @@
+export type {
+  DiagramBounds,
+  DiagramBoundsItem,
+  DiagramDirection,
+  DiagramElementKind,
+  DiagramElementRef,
+  DiagramPathHighlightMode,
+  DiagramPoint,
+  DiagramTone,
+  DiagramViewport,
+  DiagramViewportChangeReason,
+} from "./diagram-types";
+export * from "./view-state";
